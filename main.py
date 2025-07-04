649014da932a9a4bc8697dc17b288d556138252b
tiago veronesi <tveronesi@gmail.com>
2025-07-04 12:05:20 +0200

    feat: extend Movie model with additional attributes and update JSON parsing logic

--- main.py
+++ main.py
@@ -42,7 +42,9 @@ class Movie(BaseModel):
     title: str
     url: str = ""
     cover: str
+    plot: str | None = None
     release_date: str | None = None
+    languages: list[str] = []
     certificates: list[dict[str, list[str]]] = []
     directors: list[Person] = []
     cast: list[Person] = []
@@ -50,10 +52,25 @@ class Movie(BaseModel):
     duration: int | None = None
     country_codes: list[str] = []
     rating: float | None = None
+    metacritic_rating: int | None = None
     votes: int | None = None
     trailers: list[str] = []
     genres: list[str] = []
     interests : list[str] = []
+    worldwide_gross: str | None = None
+    production_budget: str | None = None
+    storyline_keywords: list[str] = []
+    filming_locations: list[str] = []
+    sound_mixes: list[str] = []
+    processes: list[str] = []
+    printed_formats: list[str] = []
+    negative_formats: list[str] = []
+    laboratories: list[str] = []
+    cameras : list[str] = []
+    aspect_ratios: list[tuple[str, str]] = []
+    summaries : list[str] = []
+    synopses : list[str] = []
+    production: list[str] = []
 
 
 
@@ -68,13 +85,17 @@ def parse_json(raw_json) -> Movie:
     data['imdb_id'] = int(data['imdbId'].replace('tt', ''))
     data['url'] = f"https://www.imdb.com/title/{data['imdbId']}/"
     data['title'] = aboveTheFoldData['originalTitleText']['text']
+    data['metacritic_rating'] = mainColumnData['metacritic']['metascore']['score'] if mainColumnData['metacritic'] else None
     data['cover'] = aboveTheFoldData['primaryImage']['url']
+    data['plot'] = mainColumnData['plot']['plotText']['plainText'] if mainColumnData['plot'] else None
     release_date = mainColumnData['releaseDate']
     data['year'] = aboveTheFoldData['releaseYear']['year']
     data['duration'] = aboveTheFoldData['runtime']['seconds'] / 60 if aboveTheFoldData['runtime'] else None
     data['rating'] = mainColumnData['ratingsSummary']['aggregateRating']
     data['votes'] = mainColumnData['ratingsSummary']['voteCount']
     data['genres'] = [genre['genre']['text'] for genre in mainColumnData['titleGenres']['genres']]
+    data['worldwide_gross'] = f"{mainColumnData['worldwideGross']['total']['amount']} {mainColumnData['worldwideGross']['total']['currency']}" if mainColumnData['worldwideGross'] else None
+    data['production_budget'] = f"{mainColumnData['productionBudget']['budget']['amount']} {mainColumnData['productionBudget']['budget']['currency']}" if mainColumnData['productionBudget'] else None
 
     trailers = mainColumnData['primaryVideos']['edges']
     data['trailers'] = [
@@ -87,11 +108,7 @@ def parse_json(raw_json) -> Movie:
     data['release_date'] = f"{release_date['year']}-{release_date['month']:02d}-{release_date['day']:02d}" if release_date else None
 
     certificates = mainColumnData['certificates']['edges']
-   # data['certificates'] = [{cert['node']['country']['id']: [cert['node']['country']['text'], cert['node']['rating']]}
-    #                        for cert in certificates if cert['node']['country']]
-
-   # movie.certificates = data['certificates']
-
+    data['certificates'] = [{cert['node']['country']['id']: [cert['node']['country']['text'], cert['node']['rating']]} for cert in certificates if cert['node']['country']]
 
     directors_dump = mainColumnData['directorsPageTitle'][0]['credits']
     data['directors'] = []
@@ -105,8 +122,51 @@ def parse_json(raw_json) -> Movie:
         c = Person.from_cast(cast_member)
         data['cast'].append(c)
 
+    filming_locations_dump = mainColumnData['filmingLocations']['edges']
+    data['filming_locations'] = [location['node']['text'] for location in filming_locations_dump]
+
+    country_codes_dump = mainColumnData['countriesDetails']['countries']
+    data['country_codes'] = [country['id'] for country in country_codes_dump]
+
+    storyline_keywords_dump = mainColumnData['storylineKeywords']['edges']
+    data['storyline_keywords'] = [keyword['node']['text'] for keyword in storyline_keywords_dump]
+
+    production_dump = mainColumnData['production']['edges']
+    data['production'] = [prod['node']['company']['companyText']['text'] for prod in production_dump]
+
+    summaries_dump = mainColumnData['summaries']['edges']
+    data['summaries'] = [summary['node']['plotText']['plaidHtml'] for summary in summaries_dump]
+
+    synopses_dump = mainColumnData['synopses']['edges']
+    data['synopses'] = [synopsis['node']['plotText']['plaidHtml'] for synopsis in synopses_dump]
+
+    sound_mixed_dump = mainColumnData['technicalSpecifications']['soundMixes']['items']
+    data['sound_mixes'] = [sound['text'] for sound in sound_mixed_dump]
+
+    processes_dump = mainColumnData['technicalSpecifications']['processes']['items']
+    data['processes'] = [process['process'] for process in processes_dump]
+
+    printed_formats_dump = mainColumnData['technicalSpecifications']['printedFormats']['items']
+    data['printed_formats'] = [format['printedFormat'] for format in printed_formats_dump]
+
+    negative_formats_dump = mainColumnData['technicalSpecifications']['negativeFormats']['items']
+    data['negative_formats'] = [format['negativeFormat'] for format in negative_formats_dump]
+
+    laboratories_dump = mainColumnData['technicalSpecifications']['laboratories']['items']
+    data['laboratories'] = [lab['laboratory'] for lab in laboratories_dump]
+
+    cameras_dump = mainColumnData['technicalSpecifications']['cameras']['items']
+    data['cameras'] = [camera['camera'] for camera in cameras_dump]
+
+    aspect_ratios_dump = mainColumnData['technicalSpecifications']['aspectRatios']['items']
+    data['aspect_ratios'] = [ ( ratio['aspectRatio'], ( ' '.join([atrb['text'] for atrb in ratio['attributes']] ))) for ratio in aspect_ratios_dump]
+
+    languages_dump = mainColumnData['spokenLanguages']['spokenLanguages']
+    data['languages'] = [lang['id'] for lang in languages_dump]
+
+
+
     movie = Movie.model_validate(data)
-    # movie.directors = data['directors']
 
     return movie
 
