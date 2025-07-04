6e80df3396b3463ac3041388c8e88c2ffd6fdef3
tiago veronesi <tveronesi@gmail.com>
2025-07-04 11:52:50 +0200

    feat: implement Pydantic models for movie and person, and add JSON parsing functions

---
+++ imdb_service/services.py
@@ -0,0 +1,36 @@
+# python
+import requests
+import json
+from lxml import html
+
+from .parsers import parse_json_movie, parse_json_search
+
+
+def fetch_movie_details(imdb_id: str):
+    url = f"https://www.imdb.com/title/tt{imdb_id}/reference"
+    resp = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    if resp.status_code != 200:
+        return None
+    tree = html.fromstring(resp.content)
+    script = tree.xpath('//script[@id="__NEXT_DATA__"]/text()')
+    if not script:
+        return None
+    raw_json = json.loads(script[0])
+    return parse_json_movie(raw_json)
+
+
+
+
+
+def search_title(title: str):
+    url = f"https://www.imdb.com/find?q={title}&s=tt&ttype=ft&ref_=fn_ft"
+    resp = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    if resp.status_code != 200:
+        return None
+    tree = html.fromstring(resp.content)
+    script = tree.xpath('//script[@id="__NEXT_DATA__"]/text()')
+    if not script:
+        return None
+    raw_json = json.loads(script[0])
+
+    return parse_json_search(raw_json)
